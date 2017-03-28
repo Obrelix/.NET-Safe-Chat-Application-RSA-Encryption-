bf21a9862d3f9a57d6b21d4b0186ff5c8e3b146b
Ioannis Alimpertis <ialimpertis@outlook.com>
2017-03-28 23:22:47 +0300

    Form Add keys Added Form Generate Keys Added

---
+++ ChatApp/ChatApp/Forms/frmGenerateKeys.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ChatApp
+{
+    partial class frmGenerateKeys
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrivate = new System.Windows.Forms.TextBox();
+            this.txtPublic = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Private Key";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Public Key";
+            // 
+            // txtPrivate
+            // 
+            this.txtPrivate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPrivate.Location = new System.Drawing.Point(12, 92);
+            this.txtPrivate.Name = "txtPrivate";
+            this.txtPrivate.Size = new System.Drawing.Size(301, 20);
+            this.txtPrivate.TabIndex = 5;
+            // 
+            // txtPublic
+            // 
+            this.txtPublic.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPublic.Location = new System.Drawing.Point(12, 41);
+            this.txtPublic.Name = "txtPublic";
+            this.txtPublic.Size = new System.Drawing.Size(301, 20);
+            this.txtPublic.TabIndex = 4;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(12, 128);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(110, 23);
+            this.btnGenerate.TabIndex = 8;
+            this.btnGenerate.Text = "Generate Keys";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            // 
+            // frmGenerateKeys
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 166);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPrivate);
+            this.Controls.Add(this.txtPublic);
+            this.MaximumSize = new System.Drawing.Size(10000, 206);
+            this.MinimumSize = new System.Drawing.Size(150, 205);
+            this.Name = "frmGenerateKeys";
+            this.Text = "Generate Keys";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrivate;
+        private System.Windows.Forms.TextBox txtPublic;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
